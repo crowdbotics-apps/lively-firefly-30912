3cea06f7413354ebd5bf718d42c6806be6b39d97
Crowdbotics <team@crowdbotics.com>
2021-09-28 02:02:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "lively_firefly_30912",
+  url: "https://lively_firefly_30912.botics.co",
+  api: "https://lively_firefly_30912.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
